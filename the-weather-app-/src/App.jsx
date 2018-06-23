acbe474d3b8266a7536d96b8734776a0d73c853c
ashwin <ashwin.ashwinkhanna@gmail.com>
2018-06-23 20:31:06 +0000

    can now parse city and country dynamically through api call

--- the-weather-app-/src/App.jsx
+++ the-weather-app-/src/App.jsx
@@ -10,7 +10,9 @@ const API_KEY = 'f7afc45d61c4404dc6b260b68ff3d063';
 class App extends React.Component {
   getWeather = async (e) => {
     e.preventDefault();
-    const api_call =  await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Toronto,Canada&appid=f7afc45d61c4404dc6b260b68ff3d063&units=metric`);
+    const city = e.target.elements.city.value
+    const country = e.target.elements.country.value
+    const api_call =  await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
     console.log(data)
   }
