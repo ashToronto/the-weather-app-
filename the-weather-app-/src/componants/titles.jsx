5067f4a3a7a6c5033419d9fa06dd2927fe2880b2
ashwin <ashwin.ashwinkhanna@gmail.com>
2018-06-23 17:51:48 +0000

    adding the titles compnant

---
+++ the-weather-app-/src/componants/titles.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Titles extends React.Component {
+  render(){
+    return
+    <div>
+      <p>Weather finder</p>
+      <p>Find out what the conditions are where you live</p>
+    </div>
+  }
+}
+
+export default titles;
--- the-weather-app-/src/index.js
+++ the-weather-app-/src/index.js
@@ -6,3 +6,11 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
+// COMPONANTS REQUIRED FOR PROJECT
+// MAIN COMP. App
+// TITLE
+// FORM
+// WRAPPER
+//
