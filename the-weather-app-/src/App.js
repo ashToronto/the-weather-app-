2adb4bc5488ba79eb13485876a2ae5438d13af8f
ashwin <ashwin.ashwinkhanna@gmail.com>
2018-06-23 17:06:17 +0000

    set-up a basic skeleton using create-react-app

---
+++ the-weather-app-/src/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class app extends React.Component {
+  render()
+    return (
+      <div>I'm refreshing my react memory with this project</div>
+    );
+}
