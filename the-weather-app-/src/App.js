9ddd338d0a756303a159507c816e4f02854c284e
ashwin <ashwin.ashwinkhanna@gmail.com>
2018-06-23 17:29:04 +0000

    web page showing edited mistakes

--- the-weather-app-/src/App.js
+++ the-weather-app-/src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 
-class app extends React.Component {
-  render()
+import './App.css';
+
+class App extends Component {
+  render() {
     return (
-      <div>I'm refreshing my react memory with this project</div>
+      <div>
+        <h1>My React app</h1>
+      </div>
     );
+  }
 }
+
+export default App;
