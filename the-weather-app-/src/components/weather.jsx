c0ef0ea7ef67d667b9d06b5d56bbc9a0793ed9d5
ashwin <ashwin.ashwinkhanna@gmail.com>
2018-06-24 17:26:31 +0000

    added new custom css template and started styling the page

--- the-weather-app-/src/components/weather.jsx
+++ the-weather-app-/src/components/weather.jsx
@@ -6,11 +6,11 @@ class Weather extends React.Component {
   render() {
     return (
       <div>
-        {this.props.temperature && <p> temperature: {this.props.temperature}</p>}
-        {this.props.city && this.props.country && <p> location: {this.props.city}, {this.props.country}</p>}
-        {this.props.humidity && <p>humidity: {this.props.humidity}</p>}
-        {this.props.description && <p>conditions: {this.props.description}</p>}
-        {this.props.error && <p> {this.props.error} </p>}
+        {this.props.temperature && <p> temperature: <span>{this.props.temperature}</span></p>}
+        {this.props.city && this.props.country && <p> location: <span>{this.props.city}, {this.props.country}</span></p>}
+        {this.props.humidity && <p>humidity: <span>{this.props.humidity}</span></p>}
+        {this.props.description && <p>conditions: <span>{this.props.description}</span></p>}
+        {this.props.error && <p> <span>{this.props.error} </span></p>}
       </div>
     );
   }
