fac8f1745745784af03f1589f9fcf1a4eb9dfb18
ashwin <ashwin.ashwinkhanna@gmail.com>
2018-06-24 18:04:10 +0000

    added a docs file

---
+++ the-weather-app-/README.md
@@ -0,0 +1,31 @@
+# A Weather app
+=====================
+
+A simple weather app powered by React. Uses the Open weather API to fetch current weather data globally.
+
+## Final Product
+
+
+## Development Dependencies
+
+* babel-core
+* babel-loader
+* babel-preset-es2015
+* babel-preset-react
+* bootstrap version 3.3.7
+* webpack
+* react
+* react-dom
+* API: openweathermap
+
+```
+
+Install the dependencies and start the server.
+
+```
+npm install
+npm create-react-app
+npm start
+open http://localhost:3000
+
+```
--- the-weather-app-/src/components/weather.jsx
+++ the-weather-app-/src/components/weather.jsx
@@ -5,12 +5,12 @@ import React from 'react';
 class Weather extends React.Component {
   render() {
     return (
-      <div>
-        {this.props.temperature && <p> temperature: <span>{this.props.temperature}</span></p>}
-        {this.props.city && this.props.country && <p> location: <span>{this.props.city}, {this.props.country}</span></p>}
-        {this.props.humidity && <p>humidity: <span>{this.props.humidity}</span></p>}
-        {this.props.description && <p>conditions: <span>{this.props.description}</span></p>}
-        {this.props.error && <p> <span>{this.props.error} </span></p>}
+      <div className= "weather__info">
+        {this.props.temperature && <p className="weather__value"> temperature: <span>{this.props.temperature}</span></p>}
+        {this.props.city && this.props.country && <p className="weather__value"> location: <span>{this.props.city}, {this.props.country}</span></p>}
+        {this.props.humidity && <p className="weather__value">humidity: <span>{this.props.humidity}</span></p>}
+        {this.props.description && <p className="weather__value">conditions: <span>{this.props.description}</span></p>}
+        {this.props.error && <p className="weather__error"> {this.props.error} </p>}
       </div>
     );
   }
